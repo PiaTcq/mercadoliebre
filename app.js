140636b59de92b964f5f0997c329e53130af89f8
PiaTcq <mertrebucq@gmail.com>
2021-05-27 01:29:38 -0300

    usando get

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-let publicPath = path.resolve(__dirname, "./public/imgs/logo-mercado-liebre.svg");
+let publicPath = path.resolve(__dirname, "public/imgs/logo-mercado-liebre.svg");
 
 app.listen(3000, () => console.log("Escuchando puerto 3000"));
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
     res.send("¡Hola, Mundo!");
 });
 app.get("/archivo", (req, res) => {
     res.sendFile(publicPath)
-})
+});
