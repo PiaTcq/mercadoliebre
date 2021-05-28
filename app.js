62c53c8880fac402723f74b53736e611b05781d4
PiaTcq <mertrebucq@gmail.com>
2021-05-28 00:27:53 -0300

    agregando estilo a mercadoliebre

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ app.use(express.static(publicPath));
 
 app.listen(3000, () => console.log("Escuchando puerto 3000"));
 
+const devolverVistas = (res, ruta) => {
+    res.sendFile(path.resolve(__dirname, ruta));
+}
 app.get("/", (req, res) => {
     res.send("¡Hola, Mundo!");
 });
-app.get("/home", (req, res) => {
-    res.sendFile(path.join(__dirname, "views/index.html"))
-});
+app.get("/home", (req, res) => devolverVistas(res, "views/index.html"));
 app.get("/ofertas", (req, res) => {
-    res.send("página de ofertas")
+    res.send("Página de ofertas")
 });
