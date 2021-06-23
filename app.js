b55f8119d32c27e76f78f5a542a8b6e0a424b3ac
PiaTcq <mertrebucq@gmail.com>
2021-06-23 04:40:21 -0300

    cambie ruta de acceso al home

--- app.js
+++ app.js
@@ -11,10 +11,7 @@ app.listen(process.env.PORT || 3000, () => console.log("Escuchando puerto 3000")
 const devolverVistas = (res, ruta) => {
     res.sendFile(path.resolve(__dirname, ruta));
 }
-app.get("/", (req, res) => {
-    res.send("¡Hola, Mundo!");
-});
-app.get("/home", (req, res) => devolverVistas(res, "views/home.html"));
+app.get("/", (req, res) => devolverVistas(res, "views/home.html"));
 app.get("/ofertas", (req, res) => {
     res.send("Acá se verá la página de ofertas")
 });
