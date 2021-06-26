75232fdb0d05cf7e66e23acab78cd055b414c511
PiaTcq <mertrebucq@gmail.com>
2021-06-26 04:29:53 -0300

    saque los get y agregue el use a las rutas base

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const rutasMaine = require("./routes/maine")
 
 let publicPath = path.resolve(__dirname, "./public");
 
@@ -8,21 +9,4 @@ app.use(express.static(publicPath));
 
 app.listen(process.env.PORT || 3000, () => console.log("Escuchando puerto 3000"));
 
-const devolverVistas = (res, ruta) => {
-    res.sendFile(path.resolve(__dirname, ruta));
-}
-app.get("/", (req, res) => devolverVistas(res, "views/home.html"));
-app.get("/ofertas", (req, res) => {
-    res.send("Acá se verá la página de ofertas")
-});
-app.get("/tiendas-oficiales", (req, res) => {
-    res.send("Acá se verá la página de tiendas oficiales")
-});
-app.get("/vender", (req, res) => {
-    res.send("Acá se verá la página vender")
-});
-app.get("/ayuda", (req, res) => {
-    res.send("Acá se verá la página de ayuda")
-});
-app.get("/register", (req, res) => devolverVistas(res, "views/register.html"));
-app.get("/login", (req, res) => devolverVistas(res, "views/login.html"));
+app.use("/", rutasMaine);
