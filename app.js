32db00ff6f2cef51b57d4dfe1e6368216756c840
PiaTcq <mertrebucq@gmail.com>
2021-06-27 23:04:06 -0300

    empezando a usar motor de renderizacion de vistas - ejs

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const rutasMain = require("./routes/main")
-
-let publicPath = path.resolve(__dirname, "./public");
-
-app.use(express.static(publicPath));
+const mainRoutes = require("./routes/main")
 
 app.listen(process.env.PORT || 3000, () => console.log("Escuchando puerto 3000"));
 
-app.use("/", rutasMain);
+app.set("view engine","ejs");
+
+let publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.use("/", mainRoutes);
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,15 +1,12 @@
-const path = require("path");
-const devolverVistas = (res, ruta) => {res.sendFile(path.resolve(__dirname, ruta))};
-
 const maineController = {
-    home : (req, res) => {devolverVistas(res, "../views/home.html")},
+    home : (req, res) => {res.render("home")},
     ofertas : (req, res) => {res.send("Acá se verá la página de ofertas")},
     tiendasOficiales : (req, res) => {res.send("Acá se verá la página de tiendas oficiales")},
     misCompras : (req, res) => {res.send("Acá se verá la página de compras")},
     vender : (req, res) => {res.send("Acá se verá la página vender")},
     ayuda : (req, res) => {res.send("Acá se verá la página de ayuda")},
-    login : (req, res) => devolverVistas(res, "../views/login.html"),
-    register : (req, res) => devolverVistas(res, "../views/register.html"),
+    login : (req, res) => {res.render("login")},
+    register : (req, res) => {res.render("register")}
 };
 
 module.exports = maineController;
